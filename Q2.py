617f24a5ed6271e2f5e4a18982564ab94acf913f
Andy Jackson <jackson.a5787@gmail.com>
2018-05-27 11:55:14 +0100

    q2a

--- Q2.py
+++ Q2.py
@@ -4,13 +4,19 @@
 
 def search (s, slst, size):
     for x in range(size):
-        if slst[x] == s:
-            return x
+        if s in slst[x]:
+            return "Grade: " + slst[x][-2:]
 
-    return -1
-
-
+    return "Student not in list"
 
 
-print(search("bog",["ape","cat","dog","bog"],4))
+
+ContinueToAsk = False
+while not ContinueToAsk:
+    name = input("enter a name : ")
+    if name == "XXX":
+        print("Good bye")
+        ContinueToAsk = True
+    else:
+        print(search(name,["JBrown 42", "MBlack 55", "SPrunty 68", "LHughes 36"],4))
 
