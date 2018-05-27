7d35d47fa886de91d1c9026ed38db1f647a1cab6
Andy Jackson <jackson.a5787@gmail.com>
2018-05-27 11:15:56 +0100

    q1c

--- Exam prep.py
+++ Exam prep.py
@@ -1,6 +1,26 @@
-y=0
-for x in range(5):
-    y+=1
-    print("\n")
-    for x in range(1,11):
-        print(x*y, end=" ")
+import random
+
+# y=0
+# for x in range(5):
+#     y+=1
+#     print("\n")
+#     for x in range(1,11):
+#         print(x*y, end=" ")
+
+
+randomno= random.randint(0,99)
+print(randomno)
+noguesses = 0
+
+correct = False
+
+while not correct:
+    inputs = int(input("guess a no between 0 - 99: "))
+    noguesses += 1
+    if inputs < randomno:
+        print(" too low. guess again ")
+    elif inputs > randomno:
+        print(" too high. guess again ")
+    else:
+        print("correct. it took you ", noguesses,  " guesses ")
+        correct = True
